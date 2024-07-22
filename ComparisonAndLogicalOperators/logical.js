b5ad71c6cba7ed502e4d88976675caed1a0a265f
Dipesh Deula <deuladipesh94@gmail.com>
2024-07-22 16:31:52 +0545

    comparision operators

---
+++ ComparisonAndLogicalOperators/logical.js
@@ -0,0 +1,14 @@
+//comparison and logical operators
+
+//Defn: Used to compare two values
+
+//Example
+let a = 5;
+let b = 10;
+
+console.log(a==b); //false
+console.log(a<b); //true
+console.log(a !==b); //true
+console.log(a=="5"); //true
+console.log(a===5); //true
+console.log(a==="5");//false
