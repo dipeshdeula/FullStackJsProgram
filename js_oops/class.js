e78cb8577be5bd735106a37dddca311d16f24114
Dipesh Deula <deuladipesh94@gmail.com>
2024-07-23 17:33:30 +0545

    intheritance updated

--- js_oops/class.js
+++ js_oops/class.js
@@ -22,6 +22,7 @@ const myCar2 = new Car("Hundai");
 console.log(myCar2);
 */
 //Ex: Static Classes
+/*
 
 class MathUtils{
     static add(a,b)
@@ -36,3 +37,30 @@ class MathUtils{
 
 console.log(MathUtils.add(5,10)); // output: 15
 console.log(MathUtils.multiply(5,10)); //output: 50
+
+*/
+
+//Inheritance
+
+//parent class
+class Car{
+    constructor(brand){
+        this.carname = brand; // Assing the value 'brand' to the 'carname' property
+    }
+    present(){
+        return 'I have a '+ this.carname;
+    }
+}
+//child class
+class Model extends Car{
+    constructor(brand,mod){
+        super(brand); //call the constructor of the parent class
+        this.model = mod;
+    }
+    show(){
+        return this.present()+',it is a '+this.model;
+    }
+}
+//creating an instance of the Model class
+let myCar = new Model("Ford","Mustang");
+console.log(myCar.show()); //Output:"I have a Ford, it is a mustang"
