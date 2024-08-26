f97fec3eb90407fdd87b0b32550563a60f563c61
Dipesh Deula <deuladipesh94@gmail.com>
2024-08-26 14:55:13 +0545

    updated

--- Dipesh_Portfolio/script.js
+++ Dipesh_Portfolio/script.js
@@ -1,44 +1,55 @@
 // Header Scroll
 let nav = document.querySelector(".navbar");
 window.onscroll = function () {
-    if(document.documentElement.scrollTop > 20){
-        nav.classList.add("header-scrolled");
-    }else{
-        nav.classList.remove("header-scrolled");
-    }
-} 
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("header-scrolled");
+  } else {
+    nav.classList.remove("header-scrolled");
+  }
+};
 
-// nav hide 
+// nav hide
 let navBar = document.querySelectorAll(".nav-link");
 let navCollapse = document.querySelector(".navbar-collapse.collapse");
-navBar.forEach(function (a){
-    a.addEventListener("click", function(){
-        navCollapse.classList.remove("show");
-    })
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
 });
 
 //for carousel image
-$(document).ready(function(){
-    $(".gallery_wrapper").slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true,
-    });
+$(document).ready(function () {
+  $(".gallery_wrapper").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+  });
 });
 
 //video carousel
-$('#videoCarousel').on('slide.bs.carousel', function () {
-    // Stop video playback on slide change
-    $(this).find('iframe').each(function () {
-        $(this).attr('src', $(this).attr('src'));
+$("#videoCarousel").on("slide.bs.carousel", function () {
+  // Stop video playback on slide change
+  $(this)
+    .find("iframe")
+    .each(function () {
+      $(this).attr("src", $(this).attr("src"));
     });
 });
 
-
 //date integration
-$(document).ready(function(){
-    $("#date").datepicker();
-    });
+$(document).ready(function () {
+  $("#date").datepicker();
+});
 
+//form-validation
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  if (!form.checkValidity()) {
+    event.preventDefault();
+  }
+  form.classList.add("was-validated");
+});
